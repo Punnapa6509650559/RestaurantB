96380d42eb1326e481164eeb76d163d49b6d47e5
Punnapa6509650559 <punnapa.tri@dome.tu.ac.th>
2025-05-03 15:59:31 +0700

    final version

--- src/main/java/com/example/restaurantB/demo/RestaurantBDemoClient.java
+++ src/main/java/com/example/restaurantB/demo/RestaurantBDemoClient.java
@@ -1,3 +1,5 @@
+// For running Java demo client
+
 package com.example.restaurantB.demo;
 
 import com.example.restaurantB.model.OrderItem;
@@ -5,17 +7,22 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Map;
 
 public class RestaurantBDemoClient {
+
     public static void main(String[] args) {
+        System.out.println("=== DEMO: Service B calling Service A ===\n");
+
         RestTemplate restTemplate = new RestTemplate();
 
-     
-        String aMenuUrl = "http://localhost:8080/menu";
-        ResponseEntity<String> menuResponse = restTemplate.getForEntity(aMenuUrl, String.class);
-        System.out.println("Menu from A:");
-        System.out.println(menuResponse.getBody());
+  
+        String menuUrl = "http://localhost:8080/menu";
+        ResponseEntity<String> menuResponse = restTemplate.getForEntity(menuUrl, String.class);
+        System.out.println("[From Service A] Menu:");
+        System.out.println("→ " + menuResponse.getBody() + "\n");
 
+   
         String calcUrl = "http://localhost:8080/calculate-price";
         List<OrderItem> order = List.of(
                 new OrderItem("ข้าวผัด", 2),
@@ -26,7 +33,11 @@ public class RestaurantBDemoClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<List<OrderItem>> request = new HttpEntity<>(order, headers);
 
-        Integer price = restTemplate.postForObject(calcUrl, request, Integer.class);
-        System.out.println("Total price from A: " + price + " THB");
+        Map<String, Object> response = restTemplate.postForObject(calcUrl, request, Map.class);
+
+        System.out.println("[From Service A] Calculated total price:");
+        System.out.println("→ Ordered items:");
+        order.forEach(item -> System.out.println("   - " + item.getName() + " x " + item.getQuantity()));
+        System.out.println("→ Total price: " + response.get("totalPrice") + " THB");
     }
 }
